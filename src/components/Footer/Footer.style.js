9e825e6375fbd9fc7f3c1289d1001c4722f591b6
Lucas Coimbra <lucas.coimbra@animaeducacao.com.br>
2024-12-03 03:06:24 -0300

    feature: Add Footer

---
+++ src/components/Footer/Footer.style.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+export const StyledFooter = styled.footer`
+  background: #F7F7F8;
+  color: #666666;
+  padding: 30px 0 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Container = styled.footer`
+  border-top: 1px solid #D0D0D0;
+  padding: 30px 0 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: 1196px;
+  width: 100%;
+`
+
+export const Label = styled.p`
+  font-size: 13px;
+  margin: 0;
+`
+
+export const PaymentList = styled.div`
+  margin: 8px -6px;
+`
+
+export const Icon = styled.img`
+  display: inline-block;
+  height: 26px;
+  margin: 6px;
+  width: 39px;
+`
+
+export const Text = styled.p`
+  margin: 0;
+  display: inline-block;
+  font-size: 11px;
+  line-height: 1.36;
+  margin-top: 4px;
+`
+
+export const ItemSecurity = styled.img`
+  margin-top: 30px;
+`
---
+++ src/components/Footer/index.js
@@ -0,0 +1,74 @@
+import React from "react";
+import {
+  Container,
+  StyledFooter,
+  Icon,
+  ItemSecurity,
+  Label,
+  PaymentList,
+  Text,
+} from "./Footer.style";
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+  const storeInfo = [
+    {
+      type: "site",
+      text: "Sterily: sterilybrasil.com",
+    },
+    {
+      type: "adress",
+      text: "Rua Governador Valadares, 582 - Centro Extrema - MG",
+    },
+    {
+      type: "name",
+      text: `© ${currentYear} STERILY BRASIL INTERMEDIACAO DE COMERCIO ELETRONICO LTDA - CNPJ: 39.996.062/0003-00`,
+    },
+    {
+      type: "contact",
+      text: "Whatsapp: (35) 99141-6513 / E-mail: sac@sterilybrasil.com",
+    },
+  ];
+
+  const icons = [
+    { slug: "billet", icon: "assets/img/payment/card-billet.svg" },
+    { slug: "amex", icon: "assets/img/payment/card-amex.svg" },
+    { slug: "visa", icon: "assets/img/payment/card-visa.svg" },
+    { slug: "diners", icon: "assets/img/payment/card-diners.svg" },
+    { slug: "mastercard", icon: "assets/img/payment/card-mastercard.svg" },
+    { slug: "discover", icon: "assets/img/payment/card-discover.svg" },
+    { slug: "aura", icon: "assets/img/payment/card-aura.svg" },
+    { slug: "hipercard", icon: "assets/img/payment/card-hipercard.svg" },
+    { slug: "elo", icon: "assets/img/payment/card-elo.svg" },
+    { slug: "hiper", icon: "assets/img/payment/card-hiper.svg" },
+    { slug: "pix", icon: "assets/img/payment/card-pix.svg" },
+  ];
+
+  return (
+    <StyledFooter>
+      <Container>
+        <Label>Formas de pagamento</Label>
+        <PaymentList>
+          {icons.map((payment) => {
+            return (
+              <Icon
+                src={payment.icon}
+                alt={payment.slug}
+                key={payment.slug}
+              />
+            );
+          })}
+        </PaymentList>
+        {storeInfo.map((info) => {
+          return <Text key={info.type}>{info.text}</Text>;
+        })}
+        <ItemSecurity
+          src="assets/img/safepayment.svg"
+          alt="Pagamento 100% Seguro"
+        />
+      </Container>
+    </StyledFooter>
+  );
+}
+
+export default Footer;
