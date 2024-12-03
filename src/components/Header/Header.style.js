371f4cc927f3f9469d5c3606444a645b7c826d68
Lucas Coimbra <lucas.coimbra@animaeducacao.com.br>
2024-12-03 03:06:36 -0300

    feature: Add Header

---
+++ src/components/Header/Header.style.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  background: #F7F7F8;
+  box-shadow: 0 0 2px 0 hsla(0,0%,80%,.5);
+  height: 80px;
+  color: #666666;
+`
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  max-width: 1196px;
+  width: 100%;
+  position: relative;
+`
+
+export const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  width: 100%;
+  padding: 12px 0;
+`
+
+export const Logo = styled.img`
+  display: block;
+  max-height: 45px;
+  max-width: 165px;
+  width: 100%;
+  object-fit: contain;
+`
+
+export const ItemSecurity = styled.img`
+  display: block;
+  float: right;
+`
---
+++ src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  Container,
+  StyledHeader,
+  ItemSecurity,
+  Logo,
+  LogoContainer,
+} from "./Header.style";
+
+function Header() {
+  return (
+    <StyledHeader>
+      <Container>
+        <LogoContainer>
+          <a href="https://www.sterilybrasil.com/">
+            <Logo src="assets/img/logo.webp" alt="Logo Sterily" />
+          </a>
+        </LogoContainer>
+        <ItemSecurity
+          src="assets/img/safepayment.svg"
+          alt="Pagamento 100% Seguro"
+        />
+      </Container>
+    </StyledHeader>
+  );
+}
+
+export default Header;
