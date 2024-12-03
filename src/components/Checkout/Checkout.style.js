26c1465a4832ca0959716eb08d5f79aa5387c2db
Lucas Coimbra <lucas.coimbra@animaeducacao.com.br>
2024-12-03 03:06:52 -0300

    feature: Add Checkout

---
+++ src/components/Checkout/Checkout.style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const StyledCheckout = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  min-height: 70vh;
+  background: #F4F6F8;
+  padding: 25px 0 45px;
+`
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  align-items: start;
+  gap: 30px;
+  max-width: 1196px;
+  width: 100%;
+`
---
+++ src/components/Checkout/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyledCheckout, Container } from './Checkout.style';
+import Identification from './Identification';
+import Delivery from './Delivery';
+import Payment from './Payment';
+import Summary from './Summary';
+
+function Checkout() {
+  return (
+    <StyledCheckout>
+      <Container>
+        <Identification />
+        <Payment />
+        <Summary />
+        <Delivery />
+      </Container>
+    </StyledCheckout>
+  )
+}
+
+export default Checkout;
